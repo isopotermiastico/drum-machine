62e1eae7ef84e96c8a41edb767ce425af9f5f690
Isopotermiastico <isopotermiastico@gmail.com>
2026-02-13 16:32:13 +0100

    Built sequencer functionality

--- script.js
+++ script.js
@@ -50,5 +50,86 @@ function createDrumGrid()
         
 }
 
+let sequencerInterval = null;
+let isPlaying = false;
+
+const sounds = 
+[
+new Audio("sounds/hihat.wav"),
+new Audio("sounds/clap.wav"),
+new Audio("sounds/snare.wav"),
+new Audio("sounds/kick.wav")
+];
+
+
+ const playButton = document.getElementById("playButton");
+
+    playButton.addEventListener("click", () => 
+    {
+        toggleSequencer();
+
+        if (isPlaying == false)
+            {
+                playButton.textContent = "play";
+            }
+        else
+            {
+                playButton.textContent = "stop";
+            }
+    })
+
+let current_step = 0
+const total_steps = 16;
+
+function playStep()
+{   
+
+    const rows = document.querySelectorAll(".pad-row");
+    rows.forEach
+    (
+        (row, row_index) =>
+        {
+            const pads = row.querySelectorAll(".drum-pad");
+            const pad = pads[current_step];
+            if (pad.classList.contains("active"))
+                {
+                    sounds[row_index].currentTime = 0;
+                    sounds[row_index].play();
+                }
+        }
+        
+        
+        
+    )
+    
+    current_step++;
+    if (current_step >= total_steps)
+    {
+        current_step = 0;
+    }
+
+}
+
+
+function toggleSequencer()
+{
+
+    if (!isPlaying)
+    {
+        current_step = 0;
+        sequencerInterval = setInterval(playStep, 200);
+        isPlaying = true;
+    }
+    else
+    {
+        clearInterval(sequencerInterval);
+        isPlaying = false;
+    }
+    
+
+}
+
+
+
 
 createDrumGrid();
